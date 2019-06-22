0861a9c82dbd9b16804b81c94322bcf9bffb181d
An Tran <tran.binhan@gmail.com>
2019-06-22 23:41:52 +0200

    Update ViewModifierComposition

--- SwiftUIMindBlowing/Scenes/MindBlowing/ViewModifierCompositionExampleView.swift
+++ SwiftUIMindBlowing/Scenes/MindBlowing/ViewModifierCompositionExampleView.swift
@@ -5,21 +5,37 @@
 import SwiftUI
 
 struct ViewModifierCompositionExampleView : View {
+
+    @State var value = 0
+
     var body: some View {
-        Text("🤯🤯")
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            withAnimation {
+                self.value += 1
+            }
+        }
+
+        return Text("🤯🤯")
             .padding()
             .background(Color.green, cornerRadius: 12)
-            .animation(.spring())
+            .rotationEffect(.degrees(30.0 * Double(self.value)))
             .padding()
             .background(Color.pink, cornerRadius: 12)
+            .rotationEffect(.degrees(25.0 * Double(self.value)))
             .padding()
             .background(Color.blue, cornerRadius: 12)
+            .rotationEffect(.degrees(20.0 * Double(self.value)))
             .padding()
             .background(Color.purple, cornerRadius: 12)
+            .rotationEffect(.degrees(15.0 * Double(self.value)))
             .padding()
             .background(Color.yellow, cornerRadius: 12)
+            .rotationEffect(.degrees(10.0 * Double(self.value)))
             .padding()
             .background(Color.red, cornerRadius: 12)
+            .rotationEffect(.degrees(5.0 * Double(self.value)))
+
     }
 }
 
